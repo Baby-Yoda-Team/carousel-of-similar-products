e9a4842fc071aab152d014a6990376cb1d5f0b08
patniza13 <albinalv2016@gmail.com>
2020-02-03 01:02:44 -0800

    added tests and some css

--- client/src/components/Slides.jsx
+++ client/src/components/Slides.jsx
@@ -1,52 +1,50 @@
 import React, { useState } from 'react';
 import data from '../dummy_data.js';
+import $ from 'jquery';
 
-// const Slides = () => {
-//   const [ slides, setSlides ] = useState([
 
-//             // "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
-//             "https://source.unsplash.com/2ShvY8Lf6l0/600x799",
-//             "https://source.unsplash.com/Dm-qxdynoEc/600x799",
-//             "https://source.unsplash.com/qDkso9nvCg0/600x799",
-//             "https://source.unsplash.com/epcsn8Ed8kY/600x799",
-//             "https://source.unsplash.com/iecJiKe_RNg/600x799",
-//             "https://source.unsplash.com/qDkso9nvCg0/600x799",
-//             "https://source.unsplash.com/2ShvY8Lf6l0/600x799",
-//             "https://source.unsplash.com/Dm-qxdynoEc/600x799",
-//             //8
-//             // 'https://via.placeholder.com/200x150?text=second',
-//             // 'https://via.placeholder.com/200x150?text=third',
-//             // 'https://via.placeholder.com/200x150?text=fourth',
-//             // 'https://via.placeholder.com/200x150?text=fifth',
-//             // 'https://via.placeholder.com/200x150?text=sixth',
-//             // 'https://via.placeholder.com/200x150?text=seventh',
-//             // 'https://via.placeholder.com/200x150?text=eighth',
-//             // 'https://via.placeholder.com/200x150?text=ninth',
-//             // 'https://via.placeholder.com/200x150?text=tenth'
 
-//   ]);
+// const items = [...data];
+// console.log(items)
+// const similarProductsData = Object.keys(JSON.parse(item));
+// console.log(similarProductsData)
+
+
+
 
   const Slides = () => {
+
+//   fetch('https://mywebsite.com/endpoint/', {
+//   method: 'POST',
+//   headers: {
+//     Accept: 'application/json',
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify({
+//     firstParam: 'yourValue',
+//     secondParam: 'yourOtherValue',
+//   }),
+// });
         //         //  currentImageIdx, setCurrentImagIdx
   const [ slides, setSlides ] = useState({data});
   const [ slideIndex, setSlideIndex ] = useState(0);
-  console.log(slides)
-  const prevSlide = () => {
+  // console.log(slides)
+  const slideBack = () => {
     // is slideIndex in the very beginning of the carousel?
                                    // true or false
     const resetToLastIndex = slideIndex === 0;
     // if slide Index is at index zero, then reset index to the last place; if false, then move index 1 spot to the left
-    const i = resetToLastIndex ? slides.length - 1 : slideIndex - 1;
-    console.log(i)
+    const i = resetToLastIndex ? slides.data.length - 1 : slideIndex - 1;
+    // console.log(i)
 
     // assign the logical index to current slide index that will be used in render method
     setSlideIndex(i);
   };
 
-  const nextSlide = () => {
+  const slideForward = () => {
      // is slideIndex in the very end of the carousel?
                             //  true or false
-    const resetToFirstIndex = slideIndex === slides.length - 1;
+    const resetToFirstIndex = slideIndex === slides.data.length - 1;
      // if slide Index is at the last index of the carousel reset index to the first place; if false, then move index 1 spot to the right
     const i = resetToFirstIndex ? 0 : slideIndex + 1;
 
@@ -55,36 +53,36 @@ import data from '../dummy_data.js';
 
   }
 
-  // create a new array with 5 elements from the slides
-  // const slideArray = slides.slice(slideIndex, slideIndex + 5);
 
-  const slideArray = slides.slice(slideIndex, slideIndex + 5);
+
+  // create a new array with 5 elements from the slides
+    const slideArray = slides.data.slice(slideIndex, slideIndex + 5);
+    console.log(slideArray)
 
   // check the length of the new array (it’s less than 5 when index is at the end of the active slides array)
   //                            true or false
    const activeSlides = slideArray.length < 5
-    // if the active Array's length is shorter than 5 slides then append missing slides from the beginning of the original slide array
-    // const activeSlides = [];
-    // if (slideArray.length < 5) {
-    //   slideArray.concat(slideArray.concat(slides.slice(0, 5 - slideArray.length)))
-    // }
+
+   // if the active Array's length is shorter than 5 slides then append missing slides from the beginning of the original slide array
+   ? [...slideArray, ...slides.data.slice(0, 5 - slideArray.length) ] : slideArray;
 
 
-
-     ? [...slideArray, ...slides.slice(0, 5 - slideArray.length) ] : slideArray;
 
 
   return (
     <div>
-      <button onClick={prevSlide}>Prev</button>
+
+      <div className="h2-style-guide st-current">Similar Products</div>
+
+       <button className="backButton" onClick={slideBack}>Back</button>
       {/* render images */}
       {activeSlides.map((slide, i) => //image, index
-        <img key={i} src={slide} alt="" style={{
-          maxWidth: '15%'
-      }} />
+        <img key={i} src={slide} alt='' style={{width: 247, height: 247}} />
       )}
-      <button onClick={nextSlide}>Next</button>
+      <button className="forwardButton" onClick={slideForward}>Forward</button>
+
     </div>
+
   );
 };
 
@@ -92,3 +90,4 @@ import data from '../dummy_data.js';
 
 
 export default Slides;
+
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,14 +3,31 @@ import ReactDOM from "react-dom";
 import Slides from "./components/Slides.jsx";
 import data from './dummy_data.js';
 
-const App = (props) => {(
-  <container>
-    <Slides />
-
-  </container>
-
-)};
 
 
-ReactDOM.render(<Slides />, document.getElementById('app'));
+
+const App = () => {
+
+
+
+  return (
+    <div className='App'>
+      <container>
+
+
+      <Slides  />
+
+      </container>
+    </div>
+  );
+}
+
+
+
+
+// ReactDOM.render(<Slides />, document.getElementById('app'));
+
+const rootElement = document.getElementById('app');
+ReactDOM.render(<Slides />, rootElement);
+
 
